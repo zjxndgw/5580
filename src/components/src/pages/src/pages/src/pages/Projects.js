92c588285a1088108d1c24d2ec94a58b88f948f0
zjxndgw <97914540+zjxndgw@users.noreply.github.com>
2025-08-16 17:43:46 +0800

    Create Projects.js

---
+++ src/components/src/pages/src/pages/src/pages/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div className="p-6">
+      <h2 className="text-2xl font-bold mb-4">项目列表 / Projects</h2>
+      <p>这里会显示所有众筹项目...</p>
+    </div>
+  );
+}
+
+export default Projects;
