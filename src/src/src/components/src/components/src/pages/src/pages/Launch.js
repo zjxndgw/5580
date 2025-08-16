e48b366a34ee4602743dbeb8eda6decd96e76ea1
zjxndgw <97914540+zjxndgw@users.noreply.github.com>
2025-08-16 20:12:24 +0800

    Create Launch.js

---
+++ src/src/src/components/src/components/src/pages/src/pages/Launch.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Launch() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>发起项目 / Launch Page</h2>
+      <p>这里是发起项目的页面。</p>
+    </div>
+  );
+}
+
+export default Launch;
