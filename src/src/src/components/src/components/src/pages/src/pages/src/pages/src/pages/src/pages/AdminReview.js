4b175af702a2d8f1ead8b7b32160c2311f4f4cba
zjxndgw <97914540+zjxndgw@users.noreply.github.com>
2025-08-16 20:13:33 +0800

    Create AdminReview.js

---
+++ src/src/src/components/src/components/src/pages/src/pages/src/pages/src/pages/src/pages/AdminReview.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AdminReview() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>管理员审核 / Admin Review</h2>
+      <p>这里是管理员审核页面。</p>
+    </div>
+  );
+}
+
+export default AdminReview;
