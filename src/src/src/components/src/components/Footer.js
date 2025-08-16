7fb00c46100d7f02046aa2a24e292de71530f902
zjxndgw <97914540+zjxndgw@users.noreply.github.com>
2025-08-16 20:11:27 +0800

    Create Footer.js

---
+++ src/src/src/components/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer style={{ padding: "10px", background: "#0f172a", color: "#fff", textAlign: "center" }}>
+      人人互助共享明天未来 © 2025
+    </footer>
+  );
+}
+
+export default Footer;
