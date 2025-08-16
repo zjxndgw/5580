3c3d11a5d3178161123958ed100e40ca15e3811e
zjxndgw <97914540+zjxndgw@users.noreply.github.com>
2025-08-16 17:42:56 +0800

    Create Home.js

---
+++ src/components/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-bold mb-4">
+        人人互助共享明天未来
+      </h1>
+      <p className="text-lg">
+        欢迎来到我们的区块链众筹平台！<br />
+        Welcome to our blockchain crowdfunding platform!
+      </p>
+    </div>
+  );
+}
+
+export default Home;
