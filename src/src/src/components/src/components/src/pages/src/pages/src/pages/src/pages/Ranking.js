fcf5f54835723837f3c0407b3a82c5b94f7fa020
zjxndgw <97914540+zjxndgw@users.noreply.github.com>
2025-08-16 20:13:13 +0800

    Create Ranking.js

---
+++ src/src/src/components/src/components/src/pages/src/pages/src/pages/src/pages/Ranking.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Ranking() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>排行榜 / Ranking</h2>
+      <p>这里显示邀请排行榜。</p>
+    </div>
+  );
+}
+
+export default Ranking;
