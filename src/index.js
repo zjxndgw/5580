9d1e934384e9c7d465c2833279a98f9443fa443a
zjxndgw <97914540+zjxndgw@users.noreply.github.com>
2025-08-16 17:26:53 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
