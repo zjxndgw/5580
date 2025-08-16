90716987f87bd243e59d697e17cf208775805d4e
zjxndgw <97914540+zjxndgw@users.noreply.github.com>
2025-08-16 17:27:27 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Launch from "./pages/Launch";
+import ProjectDetail from "./pages/ProjectDetail";
+import Login from "./pages/Login";
+import AdminPanel from "./pages/AdminPanel";
 
 function App() {
   return (
-    <div>
-      <h1>人人互助共享明天未来</h1>
-      <p>前端项目正在运行...</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/launch" element={<Launch />} />
+        <Route path="/project/:id" element={<ProjectDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminPanel />} />
+      </Routes>
+    </Router>
   );
 }
 
