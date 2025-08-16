7e6408219d1bff9d2223ab7296f7bb7168e4ce91
zjxndgw <97914540+zjxndgw@users.noreply.github.com>
2025-08-16 20:11:50 +0800

    Create Home.js

---
+++ src/src/src/components/src/components/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>人人互助共享明天未来</h1>
+      <p>前端项目正在进行中...</p>
+    </div>
+  );
+}
+
+export default Home;
